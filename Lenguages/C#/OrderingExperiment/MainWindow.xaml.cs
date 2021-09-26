ed9703728deb3351107b66b73dedf215789385b6
Carolina Pasuy <carolinapasuy@hotmail.com>
2021-09-26 12:02:04 -0500

    Implementacion en C# y Golang

--- Lenguages/C#/OrderingExperiment/MainWindow.xaml.cs
+++ Lenguages/C#/OrderingExperiment/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,12 +20,88 @@ namespace OrderingExperiment
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
+    /// var readerBig;
+    
     public partial class MainWindow : Window
     {
+        public String readerBig;
+        public String readerSmall;
+        public String tratements;
+        public StreamReader reader;
+        public int[] arrayBig;
+        public int[] arraySmall;
+        public StreamWriter sw;
+        public Stopwatch clock;
+        public double duringtime;
+        public string repetition;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            readerBig = @"C:\Users\Financiero\source\repos\Ordering-Experiment\data\BigSize.csv";
+            readerSmall = @"C:\Users\Financiero\source\repos\Ordering-Experiment\data\SmallSize.csv";
+            tratements = @"C:\Users\Financiero\source\repos\Ordering-Experiment\data\TratementsC#.csv";
+            reader = null;
+            arrayBig = new int[10000];
+            arraySmall = new int[100];
+            createArrays(readerBig, arrayBig);
+            createArrays(readerSmall, arraySmall);
+            sw = new StreamWriter(tratements, true);
+            clock = new Stopwatch();
+            duringtime = 0;
+            repetition = "";
+            tratementsAll();
+
+        }
+        public void createArrays(string path, int[] a)
+        {
             
+            string[] lines = File.ReadAllLines(path);
+
+            for (int s=0;s<lines.Length;s++)
+
+            {
+                a[s] = int.Parse(lines[s]);
+            }
+            
+        }
+        public void tratementsAll()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                clock = new Stopwatch();
+                clock.Start();
+                bubbleSort(arrayBig);
+                clock.Stop();
+                duringtime = clock.ElapsedMilliseconds;
+                repetition = repetition + duringtime + "|";
+
+                clock = new Stopwatch();
+                clock.Start();
+                bubbleSort(arraySmall);
+                clock.Stop();
+                duringtime = clock.ElapsedMilliseconds;
+                repetition = repetition + duringtime + "|";
+
+                clock = new Stopwatch();
+                clock.Start();
+                insertionSort(arrayBig);
+                clock.Stop();
+                duringtime = clock.ElapsedMilliseconds;
+                repetition = repetition + duringtime + "|";
+
+                clock = new Stopwatch();
+                clock.Start();
+                insertionSort(arraySmall);
+                clock.Stop();
+                duringtime = clock.ElapsedMilliseconds;
+                repetition = repetition + duringtime + "|";
+
+                sw.WriteLine(repetition);
+                repetition = "";
+            }
+            sw.Close();
         }
         public void bubbleSort(int [] arr)
         {
--- Lenguages/Goolang/main.go
+++ Lenguages/Goolang/main.go
@@ -1,28 +1,138 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+var readerBig string
+var readerSmall string
+var arraySmall [100]int64
+var arrayBig [10000]int64
+var repetition [1]string
+var line string
 
 func main() {
- string treatments = "data\datatreatments.csv";
+	readerBig = "C:/Users/Financiero/source/repos/Ordering-Experiment/data/BigSize.csv"
+	readerSmall = "C:/Users/Financiero/source/repos/Ordering-Experiment/data/SmallSize.csv"
+	createArraysBigs(readerBig)
+	createArraysSmalls(readerSmall)
+	tratementsAll()
 }
-func insertionSort(array []int)[]int {
-    len := len(array)
-    for i := 1; i < len; i++ {
-        for j := 0; j < i; j++ {
-            if array[j] > array[i] {
-                array[j], array[i] = array[i], array[j]
-            }
-        }
-    }
-		return array
- }
- func bubbleSort(array[] int)[]int {
-   for i:=0; i< len(array)-1; i++ {
-      for j:=0; j < len(array)-i-1; j++ {
-         if (array[j] > array[j+1]) {
-            array[j], array[j+1] = array[j+1], array[j]
-         }
-      }
-   }
-   return array
+func createArraysBigs(path string) {
+
+	data, err := os.Open(readerBig)
+	if err != nil {
+	}
+	scanner := bufio.NewScanner(data)
+
+	var s int
+	s = 0
+	for scanner.Scan() {
+		linea := scanner.Text()
+		arrayBig[s], _ = strconv.ParseInt(linea, 0, 64)
+		s++
+	}
+
+}
+func createArraysSmalls(path string) {
+
+	data, err := os.Open(readerSmall)
+	if err != nil {
+	}
+	scanner := bufio.NewScanner(data)
+
+	var s int
+	s = 0
+	for scanner.Scan() {
+		linea := scanner.Text()
+		arraySmall[s], _ = strconv.ParseInt(linea, 0, 64)
+		s++
+	}
+
+}
+func tratementsAll() {
+	file, err := os.Create("TratamentsGoolang.csv")
+	if err != nil {
+	}
+	writer := csv.NewWriter(file)
+	for i := 0; i < 100; i++ {
+
+		start := time.Now()
+		bubbleSortBig(arrayBig)
+		duration := time.Since(start)
+		duringtime := duration.Milliseconds()
+		line = line + strconv.FormatInt(int64(duringtime), 10) + "|"
+		repetition[0] = line
+
+		start = time.Now()
+		bubbleSortSmall(arraySmall)
+		duration = time.Since(start)
+		duringtime = duration.Milliseconds()
+		line = line + strconv.FormatInt(int64(duringtime), 10) + "|"
+		repetition[0] = line
+
+		start = time.Now()
+		insertionSortBig(arrayBig)
+		duration = time.Since(start)
+		duringtime = duration.Milliseconds()
+		line = line + strconv.FormatInt(int64(duringtime), 10) + "|"
+		repetition[0] = line
+
+		start = time.Now()
+		insertionSortSmall(arraySmall)
+		duration = time.Since(start)
+		duringtime = duration.Milliseconds()
+		line = line + strconv.FormatInt(int64(duringtime), 10) + "|"
+		fmt.Println("HOLA")
+		repetition[0] = line
+
+		//err = writer.Write(repetition)
+		repetition[0] = ""
+	}
+
+}
+
+func insertionSortBig(array [10000]int64) {
+	len := len(array)
+	for i := 1; i < len; i++ {
+		for j := 0; j < i; j++ {
+			if array[j] > array[i] {
+				array[j], array[i] = array[i], array[j]
+			}
+		}
+	}
+}
+
+func insertionSortSmall(array [100]int64) {
+	len := len(array)
+	for i := 1; i < len; i++ {
+		for j := 0; j < i; j++ {
+			if array[j] > array[i] {
+				array[j], array[i] = array[i], array[j]
+			}
+		}
+	}
+}
+func bubbleSortBig(array [10000]int64) {
+	for i := 0; i < len(array)-1; i++ {
+		for j := 0; j < len(array)-i-1; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+}
+func bubbleSortSmall(array [100]int64) {
+	for i := 0; i < len(array)-1; i++ {
+		for j := 0; j < len(array)-i-1; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
 }
